2701a11244178bb56baa7dd635d31dff8fc327de
Buntaro.Orita <buntaro.orita@gmail.com>
2022-08-26 13:12:05 +0900

    DP with Relaxation

--- ch05/ch05.go
+++ ch05/ch05.go
@@ -34,3 +34,28 @@ func Frog1(h []int) []int {
 func AbsInt(x int) int {
 	return int(math.Abs(float64(x)))
 }
+
+// 5.2
+// choose minimum
+func chmin(a *int, b int) {
+	if b < *a {
+		*a = b
+	}
+}
+
+// 5.3
+func Frog1Relaxation(h []int) []int {
+	dp := make([]int, len(h))
+	for i := range dp {
+		dp[i] = math.MaxInt // initialize
+	}
+
+	dp[0] = 0
+	for i := 1; i < len(h); i++ {
+		chmin(&dp[i], dp[i-1]+AbsInt(h[i]-h[i-1])) // jump from i-1 to i
+		if i > 1 {
+			chmin(&dp[i], dp[i-2]+AbsInt(h[i]-h[i-2])) // jump from i-2 to i
+		}
+	}
+	return dp
+}
--- ch05/ch05_test.go
+++ ch05/ch05_test.go
@@ -17,3 +17,15 @@ func TestFrog1(t *testing.T) {
 		t.Errorf("got %v, but want %v", got, want)
 	}
 }
+
+func TestFrog1Relaxation(t *testing.T) {
+	t.Parallel()
+
+	h := []int{2, 9, 4, 5, 1, 6, 10}
+	want := []int{0, 7, 2, 3, 5, 4, 8}
+
+	got := ch05.Frog1Relaxation(h)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, but want %v", got, want)
+	}
+}
