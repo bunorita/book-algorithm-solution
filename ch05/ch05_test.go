71455e75489cffec941c98f9b11e49aa8777f48f
Buntaro.Orita <buntaro.orita@gmail.com>
2022-08-27 11:49:33 +0900

    Refactored

--- ch05/ch05_test.go
+++ ch05/ch05_test.go
@@ -12,20 +12,30 @@ func TestFrog1(t *testing.T) {
 
 	h := []int{2, 9, 4, 5, 1, 6, 10}
 	want := []int{0, 7, 2, 3, 5, 4, 8}
-	got := ch05.Frog1(h)
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, but want %v", got, want)
+
+	tests := []*struct {
+		name  string
+		frog1 func([]int) []int
+	}{
+		{
+			name:  "pull-based",
+			frog1: ch05.Frog1,
+		},
+		{
+			name:  "relaxation",
+			frog1: ch05.Frog1Relaxation,
+		},
 	}
-}
 
-func TestFrog1Relaxation(t *testing.T) {
-	t.Parallel()
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 
-	h := []int{2, 9, 4, 5, 1, 6, 10}
-	want := []int{0, 7, 2, 3, 5, 4, 8}
-
-	got := ch05.Frog1Relaxation(h)
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, but want %v", got, want)
+			got := tt.frog1(h)
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v, but want %v", got, want)
+			}
+		})
 	}
 }
