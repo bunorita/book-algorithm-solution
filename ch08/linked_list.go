0741a8204d4becfea9b91dc7032719e22dcf02f9
Buntaro Orita <buntaro.orita@gmail.com>
2022-12-31 16:35:08 +0900

    Doubly linked list that can erases a node

--- ch08/linked_list.go
+++ ch08/linked_list.go
@@ -28,6 +28,7 @@ func (l *LinkedList) Insert(v, p *Node) {
 	if p == nil {
 		p = l.sentinel
 	}
+	// p - v
 	v.next = p.next
 	p.next = v
 }
@@ -49,6 +50,76 @@ func (l *LinkedList) GetNodeByName(name string) *Node {
 func (l *LinkedList) String() string {
 	var s string
 	for cur := l.sentinel.next; cur != nil; cur = cur.next {
+		s += cur.name + " -> "
+	}
+	return s
+}
+
+// Bidirectional Linked List
+type DoublyLinkedNode struct {
+	name       string
+	next, prev *DoublyLinkedNode
+}
+
+type DoublyLinkedList struct {
+	sentinel *DoublyLinkedNode
+}
+
+func NewDoublyLinkedList(names ...string) *DoublyLinkedList {
+	dl := &DoublyLinkedList{
+		sentinel: &DoublyLinkedNode{},
+	}
+	for i := len(names) - 1; i >= 0; i-- {
+		dl.Prepend(&DoublyLinkedNode{name: names[i]})
+	}
+	return dl
+}
+
+// insert v after p
+func (dl *DoublyLinkedList) Insert(v, p *DoublyLinkedNode) {
+	if p == nil {
+		p = dl.sentinel
+	}
+	// v - p.next
+	v.next = p.next
+	if p.next != nil {
+		p.next.prev = v
+	}
+
+	// p - v
+	p.next = v
+	v.prev = p
+}
+
+func (dl *DoublyLinkedList) Prepend(v *DoublyLinkedNode) {
+	dl.Insert(v, nil)
+}
+
+func (dl *DoublyLinkedList) Erase(v *DoublyLinkedNode) {
+	if v == nil {
+		return
+	}
+	// O(1)
+	if v.prev != nil {
+		v.prev.next = v.next
+	}
+	if v.next != nil {
+		v.next.prev = v.prev
+	}
+}
+
+func (dl *DoublyLinkedList) GetNodeByName(name string) *DoublyLinkedNode {
+	for cur := dl.sentinel.next; cur != nil; cur = cur.next {
+		if cur.name == name {
+			return cur
+		}
+	}
+	return nil
+}
+
+func (dl *DoublyLinkedList) String() string {
+	var s string
+	for cur := dl.sentinel.next; cur != nil; cur = cur.next {
 		s += cur.name + " -> "
 	}
 	return s
--- ch08/linked_list_test.go
+++ ch08/linked_list_test.go
@@ -58,3 +58,56 @@ func TestListString(t *testing.T) {
 		})
 	}
 }
+
+func TestDoublyLinkedListString(t *testing.T) {
+	t.Parallel()
+
+	tests := []*struct {
+		name  string
+		names []string
+		want  string
+	}{
+		{
+			name:  "a name",
+			names: []string{"sato"},
+			want:  "sato -> ",
+		},
+		{
+			name: "6 names",
+			names: []string{
+				"sato",
+				"suzuki",
+				"takahashi",
+				"ito",
+				"watanabe",
+				"yamamoto",
+			},
+			want: "sato -> suzuki -> takahashi -> ito -> watanabe -> yamamoto -> ",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := ch08.NewDoublyLinkedList(tt.names...).String()
+			if got != tt.want {
+				t.Errorf("got: %q, want: %q\n", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDoublyLinkedListErase(t *testing.T) {
+	t.Parallel()
+
+	lst := ch08.NewDoublyLinkedList("sato", "yamada", "suzuki")
+	yamada := lst.GetNodeByName("yamada")
+	lst.Erase(yamada)
+
+	want := "sato -> suzuki -> "
+	if got := lst.String(); got != want {
+		t.Errorf("got: %q, want: %q", got, want)
+	}
+}
