1ece71930d22d08e1672411bf2bdfe3e57edb205
Buntaro Orita <buntaro.orita@gmail.com>
2023-01-08 16:43:50 +0900

    ex 8.6 count common number pairs

--- ch08/ch08.go
+++ ch08/ch08.go
@@ -4,6 +4,7 @@ import "strconv"
 
 // ex 8.5
 // O(N+M)
+// a, b: has no duplicate values
 func CountCommonNumbers(a, b []int) int {
 	n, m := len(a), len(b)
 	ht := NewHashTable(n + m)
@@ -18,6 +19,22 @@ func CountCommonNumbers(a, b []int) int {
 		if val := ht.Get(strconv.Itoa(b[i])); val == "true" {
 			count++
 		}
+	}
+	return count
+}
+
+// ex 8.6
+// O(N+M)
+func CountCommonNumberPairs(a, b []int) int {
+	m := make(map[int]int) // number => count
+	for i := range a {     // O(N)
+		m[a[i]] += 1
+	}
+
+	count := 0
+
+	for j := range b { // O(M)
+		count += m[b[j]]
 	}
 	return count
 }
--- ch08/ch08_test.go
+++ ch08/ch08_test.go
@@ -17,3 +17,49 @@ func TestCountCommonNumbers(t *testing.T) {
 		t.Errorf("got=%d, want=%d\n", got, want)
 	}
 }
+
+func TestCountCommonNumberPairs(t *testing.T) {
+	t.Parallel()
+
+	tests := []*struct {
+		name string
+		a, b []int
+		want int
+	}{
+		{
+			name: "case1",
+			a:    []int{1},
+			b:    []int{2},
+			want: 0,
+		},
+		{
+			name: "case2",
+			a:    []int{1},
+			b:    []int{2, 1},
+			want: 1, // (i,j) = (0,1)
+		},
+		{
+			name: "case3",
+			a:    []int{1, 2, 3},
+			b:    []int{2, 2, 1, 4},
+			want: 3, // (i,j) = (0,2), (1,0), (1,1)
+		},
+		{
+			name: "case4", // swapped a, b of case3
+			a:    []int{2, 2, 1, 4},
+			b:    []int{1, 2, 3},
+			want: 3,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := ch08.CountCommonNumberPairs(tt.a, tt.b); got != tt.want {
+				t.Errorf("got=%d, want=%d\n", got, tt.want)
+			}
+		})
+	}
+}
