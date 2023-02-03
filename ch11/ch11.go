631eb74e99f18b1f6eeb7d2f4b34356355ba8bb2
Buntaro Orita <buntaro.orita@gmail.com>
2023-02-03 11:30:03 +0900

    ex 11.2 Decayed Bridges

--- ch11/ch11.go
+++ ch11/ch11.go
@@ -20,3 +20,36 @@ func Bridge(n int, edges [][2]int) int {
 	}
 	return count
 }
+
+// ex 11.2
+// https://atcoder.jp/contests/abc120/tasks/abc120_d
+func DecayedBridge(n int, edges [][2]int) []int {
+	m := len(edges)
+	counts := make([]int, m)
+
+	for i := range edges {
+		uf := NewUnionFind(n)
+		for _, edge := range edges[i+1:] {
+			uf.Unite(edge[0], edge[1])
+		}
+
+		var count int
+		unions := uf.Unions()
+		sizes := make([]int, len(unions))
+		for i, union := range unions {
+			sizes[i] = len(union)
+		}
+		for i := range sizes {
+			for j := range sizes {
+				if i < j {
+					count += sizes[i] * sizes[j]
+				} else {
+					continue
+				}
+			}
+		}
+		counts[i] = count
+	}
+
+	return counts
+}
--- ch11/ch11_test.go
+++ ch11/ch11_test.go
@@ -1,6 +1,7 @@
 package ch11_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/bunorita/book-algorithm-solution/ch11"
@@ -61,6 +62,62 @@ func TestBridge(t *testing.T) {
 			got := ch11.Bridge(tt.n, tt.edges)
 			if got != tt.want {
 				t.Errorf("got=%d, want=%d", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDecayedBridge(t *testing.T) {
+	t.Parallel()
+
+	tests := []*struct {
+		name  string
+		n     int
+		edges [][2]int
+		want  []int
+	}{
+		{
+			name: "case1",
+			n:    4,
+			edges: [][2]int{
+				{0, 1},
+				{2, 3},
+				{0, 2},
+				{1, 2},
+				{0, 3},
+			},
+			want: []int{0, 0, 4, 5, 6},
+		},
+		{
+			name: "case2",
+			n:    6,
+			edges: [][2]int{
+				{1, 2},
+				{0, 1},
+				{4, 5},
+				{2, 3},
+				{3, 4},
+			},
+			want: []int{8, 9, 12, 14, 15},
+		},
+		{
+			name: "case3",
+			n:    2,
+			edges: [][2]int{
+				{0, 1},
+			},
+			want: []int{1},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := ch11.DecayedBridge(tt.n, tt.edges)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got: %v, want: %v", got, tt.want)
 			}
 		})
 	}
