0430d1e5f64bdb8706f33dd9ea0fe8a5c373d287
Buntaro Orita <buntaro.orita@gmail.com>
2023-02-07 13:06:00 +0900

    Merge sort

--- ch12/ch12.go
+++ ch12/ch12.go
@@ -18,3 +18,46 @@ func InsertionSort(a []int) []int {
 	}
 	return a
 }
+
+func MergeSort(a []int) []int {
+	mergeSort(&a, 0, len(a))
+	return a
+}
+
+func mergeSort(a *[]int, left, right int) {
+	if right-left == 1 {
+		return
+	}
+
+	mid := (right + left) / 2
+
+	mergeSort(a, left, mid)
+	al := make([]int, mid-left)
+	copy(al, (*a)[left:mid])
+
+	mergeSort(a, mid, right)
+	ar := make([]int, right-mid)
+	copy(ar, (*a)[mid:right])
+
+	li, ri := 0, 0
+	for i := left; i < right; i++ {
+		if li == len(al) { // 左が空
+			(*a)[i] = ar[ri]
+			ri++
+			continue
+		} else if ri == len(ar) { // 右が空
+			(*a)[i] = al[li]
+			li++
+			continue
+		}
+
+		// 左右とも値がある
+		if al[li] <= ar[ri] { // 左の方が小さい
+			(*a)[i] = al[li]
+			li++
+		} else { // 右の方が小さい
+			(*a)[i] = ar[ri]
+			ri++
+		}
+	}
+}
--- ch12/ch12_test.go
+++ ch12/ch12_test.go
@@ -44,3 +44,36 @@ func TestInsertionSort(t *testing.T) {
 		})
 	}
 }
+
+func TestMergeSort(t *testing.T) {
+	t.Parallel()
+
+	tests := []*struct {
+		name string
+		a    []int
+		want []int
+	}{
+		{
+			name: "case1",
+			a:    []int{4, 2, 3, 1},
+			want: []int{1, 2, 3, 4},
+		},
+		{
+			name: "case2",
+			a:    []int{12, 9, 15, 3, 8, 17, 6, 1},
+			want: []int{1, 3, 6, 8, 9, 12, 15, 17},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := ch12.MergeSort(tt.a)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got: %v, want: %v", got, tt.want)
+			}
+		})
+	}
+}
