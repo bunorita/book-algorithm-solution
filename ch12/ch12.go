c54606450990a6ba2b608aa4fdbdd80a06168c70
Buntaro Orita <buntaro.orita@gmail.com>
2023-02-08 12:24:27 +0900

    Quick sort

--- ch12/ch12.go
+++ ch12/ch12.go
@@ -54,3 +54,35 @@ func mergeSort(a *[]int, left, right int) {
 		}
 	}
 }
+
+func QuickSort(a []int) []int {
+	quickSort(&a, 0, len(a))
+	return a
+}
+
+func quickSort(arr *[]int, left, right int) {
+	if right-left <= 1 {
+		return
+	}
+
+	a := *arr
+
+	pi := (left + right) / 2 // pivot index
+	pivot := a[pi]
+
+	// swap pivot with right edge
+	a[pi], a[right-1] = a[right-1], a[pi]
+
+	i := left // 左詰めされたpivot未満要素の右端+1（pivot以上要素の左端）
+	for j := left; j < right-1; j++ {
+		if a[j] < pivot { // pivot未満のものがあったら左に詰めていく
+			a[i], a[j] = a[j], a[i]
+			i++
+		}
+	}
+	// swap pivot with i
+	a[i], a[right-1] = a[right-1], a[i]
+
+	quickSort(arr, left, i)
+	quickSort(arr, i+1, right)
+}
--- ch12/ch12_test.go
+++ ch12/ch12_test.go
@@ -77,3 +77,31 @@ func TestMergeSort(t *testing.T) {
 		})
 	}
 }
+
+func TestQuickSort(t *testing.T) {
+	t.Parallel()
+
+	tests := []*struct {
+		name string
+		a    []int
+		want []int
+	}{
+		{
+			name: "case1",
+			a:    []int{10, 12, 15, 3, 8, 17, 4, 1},
+			want: []int{1, 3, 4, 8, 10, 12, 15, 17},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := ch12.QuickSort(tt.a)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got: %v, want: %v", got, tt.want)
+			}
+		})
+	}
+}
